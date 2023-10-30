132f940aa4db477273cdc63392d677ad1456c3ce
Avaz <almazavas@gmail.com>
2023-10-30 14:04:08 +0600

    remote new repos

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import './index.css';
 import { ThemeProvider } from '@mui/material';
 import { appTheme } from './assets/theme/theme';
-import { Test } from './layout/test/Test';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { TestAnswer } from './layout/test/TestAnswer';
 import { MainPage } from './pages/MainPage';
 
 const AppContent = () => {
   return (
     <div>
       <MainPage />
-      <Test />
-      <TestAnswer />
     </div>
   );
 };
